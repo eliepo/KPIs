7d97f6f14c654db34320ba37eb50c3c3715dcc54
ostgresql <ostgresql@atlanta-inc-desenvolvedor>
2024-01-24 18:39:23 -0300

    commit KPI

---
+++ kpi-Ib/src/main/java/com/kpisource/kpiIb/KpiIbApplication.java
@@ -0,0 +1,13 @@
+package com.kpisource.kpiIb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpiIbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpiIbApplication.class, args);
+	}
+
+}
---
+++ kpi-Ib/src/test/java/com/kpisource/kpiIb/KpiIbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpisource.kpiIb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpiIbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
